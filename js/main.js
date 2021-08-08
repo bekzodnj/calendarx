0291cdb5c8d5e7ba8bbec6bb37602d1464f3e4bd
Bekzodjon Norkuziev <bekzodnx@gmail.com>
2021-08-09 01:23:12 +0200

    feat (js, styling): Added Calendar control buttons

--- js/main.js
+++ js/main.js
@@ -6,16 +6,39 @@ const HEADER_MONTH_AND_YEAR = document.querySelector('.header-monthAndYear'),
 
 /* Date variables */
 var TODAY = new Date(),
-  MONTH = TODAY.getMonth(),
-  YEAR = TODAY.getFullYear();
+  CURRENT_MONTH = TODAY.getMonth(),
+  CURRENT_YEAR = TODAY.getFullYear();
 
-showCalendar(MONTH, YEAR);
+const MONTHS_LIST = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+showCalendar(CURRENT_MONTH, CURRENT_YEAR);
 
 /*
  *   Prepares table view for calendar
  *   @return {int}
  */
 function showCalendar(month, year) {
+  // clearing all previous cells
+  TABLE_BODY.innerHTML = '';
+
+  // settings the header and dropdown
+  HEADER_MONTH_AND_YEAR.innerHTML = MONTHS_LIST[month] + ' ' + year;
+  SELECT_YEAR.value = year;
+  SELECT_MONTH.value = month;
+
   // getDay() returns day of the week: 0-6
   let firstDayOfWeek = new Date(year, month).getDay();
 
@@ -46,6 +69,11 @@ function showCalendar(month, year) {
         let TABLE_DATA = document.createElement('td'), // <td>
           TABLE_DATA_TEXT = document.createTextNode(currDay); // text
         TABLE_DATA.appendChild(TABLE_DATA_TEXT);
+
+        if (isDateToday(currDay, month, year)) {
+          // add styling today's date-cell
+          TABLE_DATA.classList.add('bg-accent');
+        }
         TABLE_ROW.appendChild(TABLE_DATA);
         currDay++;
       }
@@ -53,6 +81,41 @@ function showCalendar(month, year) {
 
     TABLE_BODY.appendChild(TABLE_ROW);
   }
+}
+
+function next() {
+  CURRENT_YEAR = CURRENT_MONTH === 11 ? CURRENT_YEAR + 1 : CURRENT_YEAR;
+  CURRENT_MONTH = (CURRENT_MONTH + 1) % 12;
+
+  showCalendar(CURRENT_MONTH, CURRENT_YEAR);
+}
+
+function prev() {
+  CURRENT_YEAR = CURRENT_MONTH === 0 ? CURRENT_YEAR - 1 : CURRENT_YEAR;
+  CURRENT_MONTH = CURRENT_MONTH === 0 ? 11 : CURRENT_MONTH - 1;
+
+  showCalendar(CURRENT_MONTH, CURRENT_YEAR);
+}
+
+function jump() {
+  CURRENT_YEAR = parseInt(SELECT_YEAR.value);
+  CURRENT_MONTH = parseInt(SELECT_MONTH.value);
+
+  showCalendar(CURRENT_MONTH, CURRENT_YEAR);
+}
+
+function jumpToToday() {
+  showCalendar(TODAY.getMonth(), TODAY.getFullYear());
+}
+
+function isDateToday(day, month, year) {
+  let todaysDate = new Date();
+
+  return (
+    day === todaysDate.getDate() &&
+    month === todaysDate.getMonth() &&
+    year === todaysDate.getFullYear()
+  );
 }
 
 /*
